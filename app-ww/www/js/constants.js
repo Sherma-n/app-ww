4a08276de03330fd7d04759a0df411a777b7e4a3
Sherma-n <ysh.sherman@gmail.com>
2016-08-06 16:30:11 +0800

    before bower install of socket.io

--- app-ww/www/js/constants.js
+++ app-ww/www/js/constants.js
@@ -5,7 +5,7 @@ angular.module('starter')
 })
 
 .constant('API_ENDPOINT', {
-  // url: 'http://localhost:8080/api'
-  //  For a simulator use:
   url: 'http://localhost:8080/api'
+  //  For a simulator use:
+  // url: 'http://localhost:8080/api'
 });
